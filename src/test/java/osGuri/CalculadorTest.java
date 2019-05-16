67b72ac2a0381bdc777d211cc7ce9691c5ce51ca
Bruno Lippert <brunohlippert@gmail.com>
2019-05-16 10:29:28 -0300

    arrumando imports

--- src/test/java/osGuri/CalculadorTest.java
+++ src/test/java/osGuri/CalculadorTest.java
@@ -1,23 +1,24 @@
-
 package osGuri;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.DisplayName;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.*;
 
 public class CalculadorTest{
     @Test
     @DisplayName("Testa se o metodo calcula um numero de vida valido")
     public void testaCalculaNumeroDaVida() {
-        mockFormatador = mock(FormatadorImpl.class);
-        mockRedutor = mock(RedutorImpl.class);
+        FormatadorImpl mockFormatador = mock(FormatadorImpl.class);
+        RedutorImpl mockRedutor = mock(RedutorImpl.class);
         when(mockRedutor.reducaoData("25/08/2000")).thenReturn(1);
 
-
-        Calculador calculador = new FormatadorImpl(mockRedutor,mockFormatador);
+        Calculador calculador = new Calculador(mockRedutor,mockFormatador);
         int result = calculador.calculaNumeroDaVida("25/08/2000");
         assertEquals(result,1);
 
@@ -26,13 +27,13 @@ public class CalculadorTest{
     @Test
     @DisplayName("Testa se o metodo calcula um numero de destino valido")
     public void calculaNumeroDestino() {
-        mockFormatador = mock(FormatadorImpl.class);
+        FormatadorImpl mockFormatador = mock(FormatadorImpl.class);
         when(mockFormatador.formataFrase("Test")).thenReturn("Test2");
-        mockRedutor = mock(RedutorImpl.class);
-        when(reducao.reducaoData("Test2")).thenReturn(1);
+        RedutorImpl mockRedutor = mock(RedutorImpl.class);
+        when(mockRedutor.reducaoData("Test2")).thenReturn(1);
 
 
-        Calculador calculador = new FormatadorImpl(mockRedutor,mockFormatador);
+        Calculador calculador = new Calculador(mockRedutor,mockFormatador);
         int result = calculador.calculaNumeroDaVida("Teste");
         assertEquals(result,1);
 
